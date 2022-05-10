04c600ec1c1018d0b7d1da28f615bf3befed43eb
Henry Allen-Tilford <hallentilford@atlassian.com>
2022-05-10 16:02:10 -0700

    init

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Compass Orb
+
+[![CircleCI Build Status](https://circleci.com/gh/atlassian-labs/Compass-Orb.svg?style=shield 'CircleCI Build Status')](https://circleci.com/gh/atlassian-labs/Compass-Orb) [![CircleCI Orb Version](https://badges.circleci.com/orbs/atlassian/compass.svg)](https://circleci.com/orbs/registry/orb/atlassian/compass) [![Atlassian license](https://img.shields.io/badge/license-Apache%202.0-blue.svg?style=flat-square)](LICENSE) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](CONTRIBUTING.md)
+
+An orb for reporting the status of CircleCI builds and deployments to Atlassian Compass
+
+## Usage
+
+For full usage guidelines, see the [orb registry listing](http://circleci.com/orbs/registry/orb/atlassian/compass).
+
+## Contributions
+
+Contributions to Compass Orb are welcome! Please see [CONTRIBUTING.md](CONTRIBUTING.md) for details.
+
+## License
+
+Copyright (c) [YYYY] - [YYYY] Atlassian and others.
+Apache 2.0 licensed, see [LICENSE](LICENSE) file.
+
+<br/>
+
+[![With â¤ï¸ from Atlassian](https://raw.githubusercontent.com/atlassian-internal/oss-assets/master/banner-cheers.png)](https://www.atlassian.com)
---
+++ scripts/notify_deployment.sh
@@ -0,0 +1,74 @@
+run () {
+  verify_env_variables
+  generate_json_payload_deployment
+  post_to_compass
+}
+
+verify_env_variables () {
+  if [[ ! ${COMPASS_SHARED_SECRET} || ! ${COMPASS_WEBTRIGGER} ]]; then
+    "The environment variables required for the Compass orb aren’t configured. Please check if you’ve configured the integration correctly in the Compass UI."
+    exit 0
+  fi
+}
+
+generate_json_payload_deployment () {
+  iso_time=$(date '+%Y-%m-%dT%T%z'| sed -e 's/\([0-9][0-9]\)$/:\1/g')
+  echo {} | jq \
+  --arg time_str "$(date +%s)" \
+  --arg lastUpdated "${iso_time}" \
+  --arg category "${ENVIRONMENT_TYPE^^}" \
+  --arg environmentType "${ENVIRONMENT_TYPE}" \
+  --arg workflowId "${CIRCLE_WORKFLOW_ID}" \
+  --arg jobId "${CIRCLE_BUILD_NUM}" \
+  --arg status "${COMPASS_BUILD_STATUS}" \
+  '
+  ($time_str | tonumber) as $time_num |
+  {
+    "workflowId": $workflowId,
+    "jobId": $jobId,
+    "lastUpdated": $lastUpdated,
+    "status": $status,
+    "deployments": [
+      {
+          "environment": {
+            "category": $category,
+            "displayName": $environmentType,
+            "environmentId": $environmentType
+          },
+        "lastUpdated": $lastUpdated
+      }
+    ]
+  }
+  ' > /tmp/compass-status.json
+}
+
+
+post_to_compass () {
+  cat /tmp/compass-status.json
+  HTTP_STATUS=$(curl \
+  -u "${TOKEN}:" \
+  -s -w "%{http_code}" -o /tmp/curl_response.txt \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "x-forge-secret: ${COMPASS_SHARED_SECRET}" \
+  -X POST "${COMPASS_WEBTRIGGER}" --data @/tmp/compass-status.json)
+
+  echo "Results from Compass: "
+  if [ "${HTTP_STATUS}" != "200" ];then
+    echo "Error calling Compass, result: ${HTTP_STATUS}" >&2
+    jq '.' /tmp/curl_response.txt
+    exit 0
+  fi
+
+  # If reached this point, the deployment was a success.
+  echo
+  jq '.' /tmp/curl_response.txt
+  echo
+  echo
+  echo "Success!"
+}
+
+# kick off
+source ${STATE_PATH}
+run
+rm -f ${STATE_PATH}
