4019e55fff9e2bd0d432813f793b673cc6ae5bdc
Sarah Bland <sbland2@atlassian.com>
2023-06-30 15:51:48 -0500

    Updated environment name syntax

--- src/scripts/set_environment_name.sh
+++ src/scripts/set_environment_name.sh
@@ -1,3 +1,3 @@
- echo 'export ENVIRONMENT_NAME="<<parameters.environment>>"' >> $BASH_ENV
+ echo "export ENVIRONMENT_NAME=${ENVIRONMENT_NAME}" >> $BASH_ENV
     source $BASH_ENV
     echo "Using displayName: $ENVIRONMENT_NAME"
